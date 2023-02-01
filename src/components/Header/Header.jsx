28ce2eb5eb4bb382faebfac1533f67b4ac697854
avavion <avavionmvm@gmail.com>
2023-02-01 18:19:27 +0300

    Add base HTML template

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo.png";
+
+const CardButton = () => {
+    return (
+        <button>
+            <i class="fa-solid fa-cart-shopping"></i>
+            <span>1205 руб.</span>
+        </button>
+    )
+};
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <img src={logo} alt="React Sneakers Logo" />
+
+                <div>
+                    <h1>React Sneakers</h1>
+                    <p>Магазин лучших кроссовок</p>
+                </div>
+            </div>
+
+            <nav className="nav">
+                <CardButton />
+                <NavLink to="/favorites">
+                    <i class="fa-solid fa-heart"></i>
+                </NavLink>
+                <NavLink to="/orders">
+                    <i class="fa-solid fa-user"></i>
+                </NavLink>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Slide from "../components/Slider/Slide/Slide";
+import Slider from "../components/Slider/Slider";
+
+const HomePage = () => {
+    return (
+        <>
+            <Slider>
+                <Slide />
+            </Slider>
+        </>
+    )
+}
+
+export default HomePage;
